387b8fd36c8542832339f897cc596b08aeb95350
amingze <kailynnsn2@gmail.com>
2019-12-14 18:53:21 +0800

    update

---
+++ src/main/java/com/xw/crud/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package com.xw.crud.dao;
+
+import com.xw.crud.bean.Department;
+import com.xw.crud.bean.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
